c35a5decc851013a5ca04cd4c774a1b51e7d9fd7
Ashish Singh <ashishsingh.1899@gmail.com>
2023-03-01 19:44:01 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,42 @@
+const updateOutput = () => {
+  $("iframe")
+    .contents()
+    .find("html")
+    .html(
+      "<html><head><style type='text/css'>" +
+        $("#cssPanel").val() +
+        "</style></head><body>" +
+        $("#htmlPanel").val() +
+        "</body></html>"
+    );
+
+    document.getElementById("outputPanel").contentWindow.eval($("#javascriptPanel").val());
+
+};
+
+$(".toggleButton").hover(
+  function () {
+    $(this).addClass("highlightedButton");
+  },
+  function () {
+    $(this).removeClass("highlightedButton");
+  }
+);
+
+$(".toggleButton").click(function () {
+  $(this).toggleClass("active");
+
+  $(this).removeClass("highlightedButton");
+
+  var panelId = $(this).attr("id") + "Panel";
+
+  $("#" + panelId).toggle();
+});
+
+$(".panel").height($(window).height() - $("#header").height() - 20);
+
+updateOutput();
+
+$("textarea").on("change keyup paste", function () {
+  updateOutput();
+});
